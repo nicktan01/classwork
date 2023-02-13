9184f49448655c0b48a83f69631bdfa333344366
Nicholas Tan <nicholas.tan01@gmail.com>
2023-02-13 15:13:45 -0500

    Hello World in NetBeans

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Nicholas Tan
+ * email: nicholas.tan01@gmail.com
+ * date: 02/13/2023
+ * purpose: My first Hello World program in an IDE
+ */
+// Use Your Group Id value here if it is different from com.mthree
+package com.wileyedge.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
