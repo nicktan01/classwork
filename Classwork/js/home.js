d214a1466d1cf153c1ad926444100fb940463726
Nicholas Tan <nicholas.tan01@gmail.com>
2023-03-14 14:13:15 -0400

    added each

--- Classwork/js/home.js
+++ Classwork/js/home.js
@@ -4,11 +4,23 @@ $(document).ready(function(){
         type: 'GET',
         url: 'http://contactlist.us-east-1.elasticbeanstalk.com/contacts',
         success: function() {
-            alert('SUCCESS!');
+            var contactsDiv = $('#allContacts');
         },
         error: function() {
             alert('FAILURE!');
         }
+        
+    })
+    
+    $.each(contactArray, function(index, contact) {
+        var contactInfo = '<p>';
+        contactInfo += 'Name: ' + contact.firstName + ' ' + contact.lastName + '<br>';
+        contactInfo += 'Company: ' + contact.company + '<br>';
+        contactInfo += 'Email: ' + contact.email + '<br>';
+        contactInfo += 'Phone: ' + contact.phone + '<br>';
+        contactInfo += '</p><hr>';
+
+        contactsDiv.append(contactInfo);
     })
 
 })
